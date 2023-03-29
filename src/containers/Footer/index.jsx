1d289443e7f7e014a708f66fbc2c6dab893f511e
iqbalf06 <faisaelz@gmail.com>
2023-03-29 21:47:19 +0700

    initial commit

--- src/containers/Footer/index.jsx
+++ src/containers/Footer/index.jsx
@@ -1,25 +1,152 @@
 import React from "react";
-import bgfooter from "../../assets/img/footer.png";
-import { Image, Row, Col } from "react-bootstrap";
-import style from "./footer.module.css";
-import logo from "../../assets/img/logo_Edspert.png";
+// import bgfooter from "../../assets/img/footer.png";
+// import { Image, Row, Col } from "react-bootstrap";
+// import style from "./footer.module.css";
+// import logo from "../../assets/img/logo_Edspert.png";
 
 function Footer() {
   return (
+    // <>
+    //   <div className="position-relative">
+    //     <Image className={style.footer} src={bgfooter} />
+    //     <div>
+    //       <Image className={'position-absolute top-0 start-0 px-5'+ ' ' + style.logo} src={logo} />
+    //       <p className={'position-absolute top-0 start-0 px-5' + ' ' + style.address}>Jl. Watugede No.58, Wonorejo, Sariharjo, Kec. Ngaglik, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55581</p>
+    //     </div>
+    //     {/* <Row>
+    //         <p>test</p>
+    //         <Col>
+    //         <p>test</p>
+    //         </Col>
+    //     </Row> */}
+    //   </div>
+    // </>
     <>
-      <div className="position-relative">
-        <Image className={style.footer} src={bgfooter} />
-        <div>
-          <Image className={'position-absolute top-0 start-0 px-5'+ ' ' + style.logo} src={logo} />
-          <p className={'position-absolute top-0 start-0 px-5' + ' ' + style.address}>Jl. Watugede No.58, Wonorejo, Sariharjo, Kec. Ngaglik, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55581</p>
-        </div>
-        <Row>
-            <p>test</p>
-            <Col>
-            <p>test</p>
-            </Col>
-        </Row>
-      </div>
+      
+        <footer class="text-center text-lg-start bg-white text-muted">
+          <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+            <div class="me-5 d-none d-lg-block">
+              <span>Get connected with us on social networks:</span>
+            </div>
+
+            <div>
+              <a href="" class="me-4 link-secondary">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+              <a href="" class="me-4 link-secondary">
+                <i class="fab fa-twitter"></i>
+              </a>
+              <a href="" class="me-4 link-secondary">
+                <i class="fab fa-google"></i>
+              </a>
+              <a href="" class="me-4 link-secondary">
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a href="" class="me-4 link-secondary">
+                <i class="fab fa-linkedin"></i>
+              </a>
+              <a href="" class="me-4 link-secondary">
+                <i class="fab fa-github"></i>
+              </a>
+            </div>
+          </section>
+
+          <section class="">
+            <div class="container text-center text-md-start mt-5">
+              <div class="row mt-3">
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                  <h6 class="text-uppercase fw-bold mb-4">
+                    <i class="fas fa-gem me-3 text-secondary"></i>Company name
+                  </h6>
+                  <p>
+                    Here you can use rows and columns to organize your footer
+                    content. Lorem ipsum dolor sit amet, consectetur adipisicing
+                    elit.
+                  </p>
+                </div>
+
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                  <h6 class="text-uppercase fw-bold mb-4">Products</h6>
+                  <p>
+                    <a href="#!" class="text-reset">
+                      Angular
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" class="text-reset">
+                      React
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" class="text-reset">
+                      Vue
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" class="text-reset">
+                      Laravel
+                    </a>
+                  </p>
+                </div>
+
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                  <h6 class="text-uppercase fw-bold mb-4">Useful links</h6>
+                  <p>
+                    <a href="#!" class="text-reset">
+                      Pricing
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" class="text-reset">
+                      Settings
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" class="text-reset">
+                      Orders
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" class="text-reset">
+                      Help
+                    </a>
+                  </p>
+                </div>
+
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                  <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+                  <p>
+                    <i class="fas fa-home me-3 text-secondary"></i> New York, NY
+                    10012, US
+                  </p>
+                  <p>
+                    <i class="fas fa-envelope me-3 text-secondary"></i>
+                    info@example.com
+                  </p>
+                  <p>
+                    <i class="fas fa-phone me-3 text-secondary"></i> + 01 234
+                    567 88
+                  </p>
+                  <p>
+                    <i class="fas fa-print me-3 text-secondary"></i> + 01 234
+                    567 89
+                  </p>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <div
+            class="text-center p-4"
+            style={{ backgroundColor: "rgba(0, 0, 0, 0.025)" }}
+          >
+            © 2023 Copyright:
+            <a class="text-reset fw-bold" href="#">
+              Edspert.id
+            </a>
+          </div>
+        </footer>
+     
     </>
   );
 }
